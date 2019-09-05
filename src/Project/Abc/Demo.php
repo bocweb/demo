ab97416f62952918fd1c5f42982d40e2cc30693f
吴晓林2 <wuxl@weipaitang.com>
2019-09-05 14:49:29 +0800

    第一个composer

---
+++ src/Project/Abc/Demo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuxiaolin
+ * Date: 2019-09-05
+ * Time: 14:24
+ */
+namespace Project\Abc;
+class Demo
+{
+    public function index()
+    {
+        echo 'it is my first composer';
+    }
+}
